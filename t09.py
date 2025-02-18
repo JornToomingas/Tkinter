42cf86ac1d3c5b7b8c37de90a9797f2e05ff35b5
JornToomingas <jmarkus@hkhk.edu.ee>
2025-02-18 11:32:06 +0200

    Update t09.py

--- t09.py
+++ t09.py
@@ -4,21 +4,28 @@ import tkinter as tk
 
 aken = tk.Tk()
 aken.title("Raadionuppude näidis")
-
+aken.geometry("300x400")
 
 
 def show_selection():
-    print("Hind", selected_color.get())
+    print("Hind:", selected_color.get())
+    print(var1.get(), float(var2.get()), var3.get())
+    print(selected_option.get())
+    if selected_option.get()=="Kuller":
+        trans = 3
+    else:
+        trans = 0
+    summa = selected_color.get() + var1.get() + float(var1.get()) + var3.get() + trans
+    print(summa)
     
+# StringVar, mis hoiab valitud värvi nime
+selected_color = tk.IntVar(value=5)
 
 
 
-label = tk.Label(aken, text="Kasutaja ID", font=("Arial",16,"bold"), fg="black").pack()
+tk.Label(aken, text="Kasutaja ID", font="Arial,16").pack(pady=(10, 0))
+tk.Entry(aken).pack()
 
-
-#Tekst
-tekst = tk.Text(aken, wrap=tk.WORD, font=("Helvetica", 12, ), fg="black", height=1, width=10)
-tekst.pack(expand=False, fill=tk.BOTH)
 
 
 # meetod uue akna avamiseks
@@ -39,18 +46,18 @@ selected_color = tk.StringVar(value=5)
 
 
 #Loome raadionupud
-radio_red = tk.Radiobutton(aken,text="Väike (5€)", variable=selected_color, value=0)
-radio_green = tk.Radiobutton(aken,text="Suur (8€)", variable=selected_color, value=0)
-radio_blue = tk.Radiobutton(aken,text="Pere (12€)", variable=selected_color, value=0)
+tk.Label(aken, text="Vali pitsa suurus", font="Arial 14").pack()
+radio_red = tk.Radiobutton(aken,text="Väike (5€)", variable=selected_color, value=5)
+radio_green = tk.Radiobutton(aken,text="Suur (8€)", variable=selected_color, value=8)
+radio_blue = tk.Radiobutton(aken,text="Pere (12€)", variable=selected_color, value=12)
 radio_red.pack()
 radio_green.pack()
 radio_blue.pack()
 
-
 var1 = tk.IntVar(value=0)
 var2 = tk.StringVar(value=0)
 var3 = tk.IntVar(value=0)
-
+tk.Label(aken, text="Vali lisandid", font="Arial 14").pack()
 
 # Checkbox
 checkbox1 = tk.Checkbutton(aken, text="Juust (+1€)", variable=var1, onvalue=1, offvalue=0)
@@ -61,23 +68,17 @@ checkbox2.pack()
 checkbox3.pack()
 
 
+# dropdown
+tk.Label(aken, text="Vali transport", font="Arial 14").pack()
+valikud = ["Kuller", "Kohapeal"]
+selected_option = tk.StringVar()
+selected_option.set("Vali üksus")
 
+dropdown = tk.OptionMenu(aken, selected_option, *valikud)
+dropdown.pack()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+btn_confirm = tk.Button(aken, text="Kinnita valik", command=show_selection)
+btn_confirm.pack()
 
 
 
